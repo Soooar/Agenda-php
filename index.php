65474043ddf62fbe35a749ce0d7865caf7ff78e8
Soar <161715593+Soooar@users.noreply.github.com>
2024-05-02 12:01:47 -0400

    Add files via upload
    Crud em php em forma de agenda

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 // incluir a conexao na pagina e todo seu conteudo
 include 'conexao.php';
+
+if (isset($_GET['acao']) && $_GET['acao'] == 'excluir') {
+    echo "EU QUERO DELETAR ALGUEM DO MEU SISTEMA";
+    exit;
+}
+
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -22,7 +29,7 @@ include 'conexao.php';
     </header>
     <section>
         <h2>lista de contato</h2>
-        <table>
+        <table border="1">
             <thead>
                 <tr>
                     <td>ID</td>
@@ -34,6 +41,54 @@ include 'conexao.php';
                     <td>Ações</td>
                 </tr>
             </thead>
+            <tbody>
+            <?php
+                //Abrir a conexão com o banco de dados
+                $conexaoComBanco = abrirBanco();
+                //Preparar a consulta SQL para selecionar os dados no BD
+                $query = "SELECT id, nome, sobrenome, nascimento, endereco, telefone FROM pessoas"; 
+                //Executar a query (o sql no banco)
+                $result = $conexaoComBanco->query($query);
+    
+                // echo "<pre>";
+                // print_r($registro);
+                // echo "</pre>";
+                // exit
+
+                //($registro = $result->fetch_assoc()
+
+                //Verificar se a query retornou registros
+                 if ($result->num_rows > 0) {
+                    //tem registro no banco
+                    while ($registro = $result->fetch_assoc()) {                                                  
+                   ?>
+                    <tr>
+                        <td><?= $registro['id']?></td>
+                        <td><?= $registro['nome'] ?></td>
+                        <td><?= $registro['sobrenome'] ?></td>
+                        <td><?= date("d/m/Y", strtotime($registro['nascimento']))?></td>
+                        <td><?= $registro['endereco'] ?></td>
+                        <td><?= $registro['telefone'] ?></td>
+                        <td>
+                            <a href="#"><button>Editar</button></a>
+                            <a href="?acao=excluir&id=<?=$registro['id'] ?>"
+                             onclick="return confirm('tem certeza que deseja excluir?');"><button>Excluir</button></a>
+                        </td>
+                    </tr>
+                    <?php
+                    }
+
+                 } else {
+                    ?>
+                    <tr>
+                        <td colspan='7'>Nenhum Registro Salvo No Banco de Dados</td>
+                    </tr>
+                    <?php
+                 }
+                //Criar um laço de repetição para preencher a tabela
+
+            ?>
+            </tbody>
         </table>
     </section>
 </body>
